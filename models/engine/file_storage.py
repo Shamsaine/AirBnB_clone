511b254bd48dbcd31f32563e8dbd407a88a44e43
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-03-09 14:49:50 +0100

    added new file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,24 @@
 # class that controls the storage of objects in files
 import json
 
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.user import User
 from models.base_model import BaseModel
 
 class FileStorage:
     __file_path = "file.json"
-    __objects = {}
+    __objects = {
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review,
+        "User": User
+    }
 
     def all(self):
         return self.__objects
