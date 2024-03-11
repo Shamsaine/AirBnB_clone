c789cf39687fd37d3b15de7f4de6eeaed4844a7d
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-03-10 17:49:33 -0700

    added new file

--- models/review.py
+++ models/review.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # class for handling reviewsfrom models.base_model import BaseModel
 from models.base_model import BaseModel
+
+
 class Review(BaseModel):
     """Review class that inherits from BaseModel"""
     place_id = ""
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 # class for the creation of users
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """User class that inherits from BaseModel"""
     email = ""
